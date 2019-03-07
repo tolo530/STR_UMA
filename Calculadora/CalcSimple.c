40c31fe572a7f794050ebe15608a3f4d2eb3e84d
Carlos BR <tolo130@hotmail.com>
2019-03-08 00:01:26 +0100

    Cambios en estructura y funciones

--- Calculadora/CalcSimple.c
+++ Calculadora/CalcSimple.c
@@ -1,19 +1,11 @@
 #include <stdio.h>
-
-int F_OR(int* valor1, int* valor2);         //Hace la operación OR de los parametros
-int F_AND(int* valor1, int* valor2);        //Hace la operación AND de los parametros
-int F_XOR(int* valor1, int* valor2);        //Hace la operación XOR de los parametros
-int F_DIZ(int* valor1);                     //Desplaza a la izquerda el parametro
-int F_DDE(int* valor1);                     //Desplaza a la derecha el parametro
-int F_CF(int* valor1);                      //Cambia un valor a otro formato (binario o hexadecimal)
-void F_Lectura1 (int *valor1);              //Lee un valor
-void F_Lectura2 (int *valor1, int *valor2); //Lee dos valores
-
+#include "Mis_Funciones.h"
 
 int main (){
 
     int opcion;
     int valor1, valor2, res;
+    int formato=1; //Hexadecimal por defecto
 
     printf ("Introduce el tipo de operacion:\n"
               "1.- OR Logico\n"
@@ -22,14 +14,13 @@ int main (){
               "4.- << (Desplazamiento a la izquierda)\n"
               "5.- >> (Desplazamiento a la derecha)\n"
               "6.- Cambio de formato (0-> binario 1->hexadecimal)\n"
-              "0.- Salir\n"
-              "Tipo>> ");
-    
-    
+              "7.- Muestra este menu otra vez\n"
+              "0.- Salir\n");
+              
    
 
     do{
-
+        printf("Tipo>> ");
         scanf ("%d", &opcion);
 
         switch (opcion){
@@ -41,44 +32,59 @@ int main (){
              case 1:
                  //Llamar función OR
                  F_Lectura2(&valor1, &valor2);
-                 res=F_OR(&valor1, &valor2);
+                 res=F_OR(&valor1, &valor2, &formato);
                  printf("%d OR %d = %d\n", valor1, valor2, res);
                  break;
 
              case 2:
                  //Llamar función AND
                  F_Lectura2(&valor1, &valor2);
-                 res=F_AND(&valor1, &valor2);
+                 res=F_AND(&valor1, &valor2, &formato);
                  printf("%d AND %d = %d\n", valor1, valor2, res);
                 break;
 
              case 3:
                 //Llamar función xor;
                 F_Lectura2(&valor1, &valor2);
-                res=F_XOR(&valor1, &valor2);
+                res=F_XOR(&valor1, &valor2, &formato);
                 printf("%d XOR %d = %d\n", valor1, valor2, res);
                 break;
 
              case 4:
                  //Llamar función <<
                  F_Lectura1(&valor1);
-                 res=F_DIZ(&valor1);
+                 res=F_DIZ(&valor1, &formato);
                  printf("%d << = %d\n", valor1, res);
                  break;
 
              case 5:
                  //Llamar función >>
                  F_Lectura1(&valor1);
-                 res=F_DDE(&valor1);
+                 res=F_DDE(&valor1, &formato);
                  printf("%d >> = %d\n", valor1, res);
                  break;
 
              case 6:
                  //Llamar función cambio de formato
+                 F_CF(&formato);
+                 break;
+                 
+             case 7:
+                 //Muestra la ayuda
+                 printf ("Operaciones:\n"
+                          "1.- OR Logico\n"
+                          "2.- AND Logico\n"
+                          "3.- XOR Logico\n" 
+                          "4.- << (Desplazamiento a la izquierda)\n"
+                          "5.- >> (Desplazamiento a la derecha)\n"
+                          "6.- Cambio de formato (0-> binario 1->hexadecimal)\n"
+                          "7.- Muestra este menu otra vez\n"
+                          "0.- Salir\n");
+                 break;
     
              default:
-                 printf("ERROR, introduce una operación valida\n"
-                        "Tipo>> ");
+                 printf("ERROR, introduce una operación valida\n");
+                        
                  
     }
 
---
+++ Calculadora/Mis_Funciones.h
@@ -0,0 +1,8 @@
+int F_OR(int* valor1, int* valor2, int* formato);         //Hace la operación OR de los parametros
+int F_AND(int* valor1, int* valor2, int* formato);        //Hace la operación AND de los parametros
+int F_XOR(int* valor1, int* valor2, int* formato);        //Hace la operación XOR de los parametros
+int F_DIZ(int* valor1, int* formato);                     //Desplaza a la izquerda el parametro
+int F_DDE(int* valor1, int* formato);                     //Desplaza a la derecha el parametro
+void F_CF(int* formato);                                  //Cambia un valor a otro formato (binario o hexadecimal)
+void F_Lectura1 (int *valor1);                            //Lee un valor
+void F_Lectura2 (int *valor1, int *valor2);               //Lee dos valores
